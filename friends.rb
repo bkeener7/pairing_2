407e4938b5a83acc61db39d262eaac4d336d82fd
Bryan Keener <bkeener7@gmail.com>
2022-08-03 14:46:58 -0600

    Add places

--- friends.rb
+++ friends.rb
@@ -2,3 +2,6 @@ friends = ["Steve", "Joe", "Karen", "Tim", "Bob"]
 puts friends[2]
 puts friends[4]
 puts friends[0]
+places = ["Maui", "Tokyo", "Minnesota", "Colorado", "California"]
+places.shift
+puts places
